2c21eb44bf4928dda6a76e8c72aa716ad217f007
saikiranagiru <84036204+saikiranagiru@users.noreply.github.com>
2021-11-17 22:17:09 -0600

    Created pug files and updated comic.js

--- controller/costume.js
+++ controller/costume.js
@@ -1,30 +1,29 @@
-var Costume = require('../models/costume');
-
+var Costume = require('../models/costume'); 
+ 
 // List of all Costumes 
-exports.costume_list = async function (req, res) {
-    try {
-        theCostumes = await Costume.find();
-        res.send(theCostumes);
-    }
-    catch (err) {
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }
-};
-
-
-// for a specific Costume.
-exports.costume_detail = async function(req, res) {
-    console.log("detail" + req.params.id)
-    try {
-    result = await Costume.findById( req.params.id)
-    res.send(result)
-    } catch (error) {
-    res.status(500)
-    res.send(`{"error": document for id ${req.params.id} not found`);
-    }
-   };
-
+exports.costume_list = async function(req, res) { 
+    try{ 
+        theCostumes = await Costume.find(); 
+        res.send(theCostumes); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }  
+}; 
+ 
+// for a specific Costume. 
+exports.costume_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Costume.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
+ 
 // Handle Costume create on POST. 
 exports.costume_create_post = async function(req, res) { 
     console.log(req.body) 
@@ -45,35 +44,41 @@ exports.costume_create_post = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
-
+ 
 // Handle Costume delete form on DELETE. 
-exports.costume_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
-};
+exports.costume_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Costume.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
+ 
+// Handle Costume update form on PUT. 
+exports.costume_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Costume.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.costume_type)  
+               toUpdate.costume_type = req.body.costume_type; 
+        if(req.body.cost) toUpdate.cost = req.body.cost; 
+        if(req.body.size) toUpdate.size = req.body.size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 
-// Handle Costume update form on PUT.
-exports.costume_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.id} with body
-   ${JSON.stringify(req.body)}`)
-    try {
-    let toUpdate = await Costume.findById( req.params.id)
-    // Do updates of properties
-    if(req.body.costume_type)
-    toUpdate.costume_type = req.body.costume_type;
-    if(req.body.cost) toUpdate.cost = req.body.cost;
-    if(req.body.size) toUpdate.size = req.body.size;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
-    } catch (err) {
-    res.status(500)
-    res.send(`{"error": ${err}: Update for id ${req.params.id}
-   failed`);
-    }
-   };
-
-// VIEWS 
-// Handle a show all view 
 exports.costume_view_all_Page = async function(req, res) { 
     try{ 
         theCostumes = await Costume.find(); 
@@ -84,3 +89,51 @@ exports.costume_view_all_Page = async function(req, res) {
         res.send(`{"error": ${err}}`); 
     }   
 }; 
+
+exports.costume_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Costume.findById( req.query.id) 
+        res.render('costumedetail',  
+{ title: 'Costume Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+exports.costume_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('costumecreate', { title: 'Costume Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+exports.costume_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Costume.findById(req.query.id) 
+        res.render('costumeupdate', { title: 'Costume Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+exports.costume_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Costume.findById(req.query.id) 
+        res.render('costumedelete', { title: 'Costume Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
